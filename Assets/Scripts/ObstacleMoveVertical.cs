12b2e12b9935bd15f31d92b49d4e703414665875
Benoit Q <benoit.quero@etudiant.univ-rennes1.fr>
2019-05-05 16:13:07 +0200

    obstacles mouvants

---
+++ Assets/Scripts/ObstacleMoveVertical.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMoveVertical : MonoBehaviour
+{
+	public Transform _transform;
+
+	int sens = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		_transform.Translate(0, sens * 2 * Time.deltaTime, 0);
+		if (_transform.position.y>11)
+		{
+			sens = -1; 
+		}
+		if (_transform.position.y < 6)
+		{
+			sens = 1;
+		}
+
+    }
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+	Text text;
+	float time;
+	bool play = true;
+
+	public Transform player;
+	public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		text = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (player.transform.position.x== destination.transform.position.x)
+		{
+			play = false;
+		}
+
+		if (play)
+		{
+			time += 100 * Time.deltaTime;
+			string cent = Mathf.Floor(time % 100).ToString("00");
+			string seconds = (Mathf.Floor(time / 100) % 60).ToString("00");
+			string minutes = Mathf.Floor((time/100 % 3600)/60).ToString("00");
+			text.text = minutes + ":" + seconds + ":" + cent;
+		}
+
+    }
+}
